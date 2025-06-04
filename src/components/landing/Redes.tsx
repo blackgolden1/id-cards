074a4f0ce33697c3f24d28296170b8fb225e2dd1
sarasarixsara <sara.cuy.ramirez@gmail.com>
2025-06-04 16:34:00 -0500

    social network links

--- src/components/landing/Redes.tsx
+++ src/components/landing/Redes.tsx
@@ -7,7 +7,7 @@ interface Network {
 export default function Redes({ name, url, icon }: Network) {
     return (
 <section className="w-full mx-auto flex flex-col items-center">
-    <a href={url} className="w-fit mx-auto text-center">
+    <a href={url} className="w-fit mx-auto text-center" target="_blank">
         <div className="rounded-full p-1 w-fit ">
             <img src={icon} alt="" className="md:w-[7rem] w-[3rem]"/>
         </div>
